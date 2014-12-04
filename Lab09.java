7f1421d2235add4cc619731ff86ff609a132d256
rebecca-brunsberg <rebeccabrunsberg@unknown-e0-f8-47-15-a7-18.home>
2014-12-04 21:01:27 +0000

    finished lab09

--- Lab09.java
+++ Lab09.java
@@ -2,8 +2,8 @@
  * A robot moves along a row, collecting piles of beepers and placing them one square to
  * the right.
  * 
- * @author <...>
- * @version <...>
+ * @author Rebecca brunsberg
+ * @version 4th December 2014
  *
  */
 
@@ -11,13 +11,32 @@ import edu.fcps.karel2.Display;
 import javax.swing.JOptionPane;
 
 public class Lab09 {
-	 
-	 public static void main(String[] args) {
-		 String map = JOptionPane.showInputDialog("Which map?");
-		 Display.openWorld("maps/"+map+".map");
-		 Display.setSize(10, 10);
-		 Athlete athena = new Athlete(1, 1, Display.EAST, 0);
-		 //TODO write a combination of definite and indefinite loops that will cause athena
-		 // to pick up each pile of beepers and deposit them one square to the right.
-	 }
+  
+  public static void main(String[] args) {
+   String map = JOptionPane.showInputDialog("Which map?");
+   Display.openWorld("maps/"+map+".map");
+   Display.setSize(10, 10);
+   Athlete athena = new Athlete(1, 1, Display.EAST, 0);
+   //TODO write a combination of definite and indefinite loops that will cause athena
+   // to pick up each pile of beepers and deposit them one square to the right.
+ 
+   int beepersUp=0;
+   int beepersDown=0;
+   for (int space=1; space<=8; space++){
+     while(athena.nextToABeeper()){
+       athena.pickBeeper();
+       beepersUp++;
+     }
+     while (beepersDown>0){
+       athena.putBeeper();
+       beepersDown--;
+     }
+     athena.move();
+     beepersDown=beepersUp;
+     beepersUp=0;
+     
+   }
+   
+  }
  }
+  
